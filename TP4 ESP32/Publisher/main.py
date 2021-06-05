5cb8155e70b2c265950bc7c6b5b194046f16c2dd
stefano Sossi <tenosossi2012@gmail.com>
2021-06-05 14:42:40 -0400

    Pushing publisher code

---
+++ TP4 ESP32/Publisher/main.py
@@ -0,0 +1,27 @@
+def connect():
+  global client_id, mqtt_server, topic_sub
+  client = MQTTClient(client_id, mqtt_server)
+  client.connect()
+  print('Connected to %s MQTT broker, subscribed to %s topic' % (mqtt_server, topic_sub))
+  return client
+
+def restart_and_reconnect():
+  print('Failed to connect to MQTT broker. Reconnecting...')
+  time.sleep(10)
+  machine.reset()
+
+try:
+  client = connect()
+except OSError as e:
+  restart_and_reconnect()
+
+while True:
+  try:
+    client.check_msg()
+    if (time.time() - last_message) > message_interval:
+      msg = b'vini hola como estas hermano kek #%d' % counter
+      client.publish(topic_pub, msg)
+      last_message = time.time()
+      counter += 1
+  except OSError as e:
+    restart_and_reconnect()
