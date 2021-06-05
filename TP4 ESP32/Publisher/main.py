05000b7aef005c59f9a417531dc5491256b112a0
stefano Sossi <tenosossi2012@gmail.com>
2021-06-05 15:50:05 -0400

    Pushing oled code uploaded

--- TP4 ESP32/Publisher/main.py
+++ TP4 ESP32/Publisher/main.py
@@ -1,8 +1,10 @@
+from time import sleep
+from boot import scroll_in_screen
 def connect():
-  global client_id, mqtt_server, topic_sub
+  global client_id, mqtt_server, topic_pub
   client = MQTTClient(client_id, mqtt_server)
   client.connect()
-  print('Connected to %s MQTT broker, subscribed to %s topic' % (mqtt_server, topic_sub))
+  print('Connected to %s MQTT broker, subscribed to %s topic' % (mqtt_server, topic_pub))
   return client
 
 def restart_and_reconnect():
@@ -20,6 +22,11 @@ while True:
     client.check_msg()
     if (time.time() - last_message) > message_interval:
       msg = b'vini hola como estas hermano kek #%d' % counter
+      global oled, i2c_rst
+      oled.fill(0)
+      oled.show()
+      screen2 =[[0,30, msg]]
+      scroll_in_screen(screen2)
       client.publish(topic_pub, msg)
       last_message = time.time()
       counter += 1
