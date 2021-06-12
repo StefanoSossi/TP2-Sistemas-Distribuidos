85c363e89ee9a1cb18f99cdca6a425692f642de0
stefano Sossi <tenosossi2012@gmail.com>
2021-06-12 15:54:16 -0400

    Pushing esp32 code

---
+++ TP5/ESP32/boot.py
@@ -0,0 +1,97 @@
+import time
+import ntptime
+from umqttsimple import MQTTClient
+import ubinascii
+import machine
+import micropython
+import network
+import esp
+import utime
+from machine import Pin, I2C
+import ssd1306
+from time import sleep
+esp.osdebug(None)
+import gc
+gc.collect()
+
+oled_width = 128
+oled_height = 64
+i2c_rst = Pin(16, Pin.OUT)
+# Initialize the OLED display
+i2c_rst.value(0)
+time.sleep_ms(5)
+i2c_rst.value(1)
+
+i2c_scl = Pin(15, Pin.OUT, Pin.PULL_UP)
+i2c_sda = Pin(4, Pin.OUT, Pin.PULL_UP)
+
+i2c = I2C(scl=i2c_scl, sda=i2c_sda)
+# Create the display object
+oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
+
+led = Pin(25, Pin.OUT)
+
+
+ssid = 'Room123'
+password = 'SOstefanoSSI47779'
+mqtt_server = 'research.upb.edu'
+#EXAMPLE IP ADDRESS
+#mqtt_server = '192.168.1.144'
+client_id = ubinascii.hexlify(machine.unique_id())
+master_sub = b'upb/master/response'
+master_pub = b'upb/master/request'
+worker_sub = ''
+worker_pub = ''
+
+
+last_message = 0
+message_interval = 5
+counter = 0
+
+station = network.WLAN(network.STA_IF)
+station.active(True)
+
+oled.fill(0)
+
+print("Available networks")
+print(station.scan())
+
+availalblenetworks = str(station.scan())
+#oled.text(str(station.scan()), 0, 0)
+        
+
+station.connect(ssid, password)
+
+while station.isconnected() == False:
+  pass
+
+# This use Epoch time of January 1, 2000. This is 946,684,800 seconds later than UNIX Epoch time of January 1, 1970.
+ntptime.settime()         # This library call sets RTC to ntp time.
+machine.RTC().datetime()  # Read the hardware RTC in datetime format.
+timestamp = 946684800 + utime.time() # Unis epoch time
+timestampid = str(timestamp)[-5:]
+
+
+print('Connection successful  ')
+
+ipsplited = station.ifconfig()[0].split('.')
+idesp32 = ipsplited[2] + "." + ipsplited[3] + "." +str(timestampid)
+#print('id : ' + idesp32)
+#oled.text(str(station.ifconfig()), 0, 10)
+workerid = ""
+
+screen1 = [[0,0, availalblenetworks],[0,16, str(station.ifconfig())]]
+
+def scroll_in_screen(screen):
+  for i in range (0, oled_width+1, 4):
+    for line in screen:
+      oled.text(line[2], -oled_width+i, line[1])
+    oled.show()
+    if i!= oled_width:
+      oled.fill(0)
+
+
+scroll_in_screen(screen1)
+
+#oled.show()
+
---
+++ TP5/ESP32/main.py
@@ -0,0 +1,145 @@
+from time import sleep
+from boot import scroll_in_screen
+import json
+
+
+##Publisher
+
+def restart_and_reconnect():
+  msg = 'Failed to connect to MQTT broker. Reconnecting...'
+  print( msg )
+  oled.fill(0)
+  oled.show()
+  screen2 =[[0,30, msg]]
+  scroll_in_screen(screen2)
+  time.sleep(10)
+  machine.reset()
+
+def request_worker()
+  try:
+    client.check_msg()
+    #msg al master ==> {sensor_id : "", worker : "" }
+    global oled, idesp32, workerid
+    msg = b'{ sensor_id : %s, worker : %s }' % (idesp32, workerid)
+    msgoled = b'Request worker to master'
+    print(msgoled)
+    oled.fill(0)
+    oled.show()
+    screen2 =[[0,30, msgoled]]
+    scroll_in_screen(screen2)
+
+    client.publish(master_pub, msg)
+  except OSError as e:
+    restart_and_reconnect()
+
+def request_work()
+  try:
+    #client.check_msg()
+    #msg al worker ==> {sensor_id : "" }
+    global oled, i2c_rst, idesp32, workerid
+    msg = b'{ sensor_id : %d}' % idesp32
+    msgoled = b'Request work to worker'
+    oled.fill(0)
+    oled.show()
+    screen2 =[[0,30, msgoled]]
+    scroll_in_screen(screen2)
+    client.publish(worker_pub, msg)
+  except OSError as e:
+    restart_and_reconnect()
+
+def work_led(freq, iteration)
+  global oled, i2c_rst, idesp32, workerid, led
+  i = 0
+  msgoled = b'working'
+  oled.fill(0)
+  oled.show()
+  screen2 =[[0,30, msgoled]]
+  scroll_in_screen(screen2)
+  while i <= iteration:
+    led.value(1)
+    sleep(freq)
+    led.value(0)
+    
+##Subscriber
+
+def connect():
+  global client_id, mqtt_server
+  client = MQTTClient(client_id, mqtt_server)
+  client.set_callback(sub_cb)
+  client.connect()
+
+def subscribe_master():
+  connect()
+  client.subscribe(master_sub)
+  print('Connected to %s MQTT Master, subscribed to %s topic' % (mqtt_server, master_sub))
+  request_worker()
+  return client
+
+def subscribe_Worker():
+  connect()
+  client.subscribe(worker_sub)
+  print('Connected to %s MQTT Worker, subscribed to %s topic' % (mqtt_server, worker_sub))
+  request_work()
+  return client
+
+def sub_cb(topic, msg):
+  global oled, i2c_rst, led, idesp32, workerid, master_sub, worker_pub, worker_sub
+  
+  oled.fill(0)
+  oled.show()
+  screen2 =[[0,30, msg]]
+  scroll_in_screen(screen2)
+  
+  print((topic, msg))
+  if topic == master_sub:
+    #msg del master ==> {destination = "", worker : "" }
+    print(msg)
+    oled.fill(0)
+    oled.show()
+    screen2 =[[0,30, msg]]
+    scroll_in_screen(screen2)
+    msgjson = json.loads(msg)
+    if msgjson["destination"] == idesp32:
+      print('Worker assigned')
+      msgoled = b'Worker assigned'
+      oled.fill(0)
+      oled.show()
+      screen2 =[[0,30, msgoled]]
+      scroll_in_screen(screen2)
+      workerid = msgjson["worker"]
+      worker_sub = b'upb/%d/response' % workerid
+      worker_pub = b'upb/%d/request' % workerid
+      try:
+        client = subscribe_Worker()
+      except OSError as e:
+        restart_and_reconnect()
+      
+  if topic == worker_sub:
+    #msg del master ==> {destination = "", worker : "" }
+    print('Work assigned')
+    msgoled = b'Work assigned'
+    oled.fill(0)
+    oled.show()
+    screen2 =[[0,30, msgoled]]
+    scroll_in_screen(screen2)
+    msgjson = json.loads(msg)
+    freq = msgjson["freq"]
+    iteration = msgjson["iteration"]
+    work_led()
+    try:
+      client = subscribe_master()
+    except OSError as e:
+      restart_and_reconnect()
+
+
+
+try:
+  client = subscribe_master()
+except OSError as e:
+  restart_and_reconnect()
+
+while True:
+  try:
+    client.check_msg()
+  except OSError as e:
+    restart_and_reconnect()
