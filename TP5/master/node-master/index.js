3055585b8068954d4793dc18c4fcc6198d7c3d98
VntValenzuela <vincent.valenzuela@live.com>
2021-06-15 18:54:29 -0400

    Bug fixes on mqtt master

--- TP5/master/node-master/index.js
+++ TP5/master/node-master/index.js
@@ -1,6 +1,6 @@
 const mqtt = require('mqtt');
 const MongoClient = require('mongodb').MongoClient;
-const url = "mongodb://mongo:27017/tp5-db";
+const url = "mongodb://localhost:27017/tp5-db";
 const http = require('http');
 
 //create a server object:
@@ -23,93 +23,48 @@ client.on('connect', () => {
     client.subscribe(topic)
 })
 
-MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db("tp5-db");
-    dbo.createCollection("workers", function(err, res) {
-      if (err) throw err;
-      console.log("Collection created!");
-      db.close();
-    });
-  });
-
-MongoClient.connect(mongoUri, function(error, database) {
+MongoClient.connect(url, function(error, database) {
     if(error != null) {
         throw error;
     }
-
-    var collection = database.collection("workers");
-    collection.createIndex( { "topic" : 1 } );
+    var dbo = database.db("tp5-db");
+    dbo.createCollection("workers", function(error, res) {
+        if (error) throw error;
+        console.log("Collection created!");
+      });
+    var collection = dbo.collection("workers");
+   // collection.createIndex( { "topic" : 1 } );
 
     client.on('message', function (topic, message) {
-        var messageObject = {
-            topic: topic,
-            message: message.toString()
-        };
-
-        collection.insert(messageObject, function(error, result) {
-            if(error != null) {
-                console.log("ERROR: " + error);
-            }
-        });
+        if(topic == 'upb/master/register'){
+            console.log("[Received] topic: " + topic.toString());  //Print topic name
+            console.log("[Received] message: " + message.toString()); //Print payload
+            collection.insert(message, function(error, result) {
+                if(error != null) {
+                    console.log("ERROR: " + error);
+                }
+            });
+        }
+        else if(topic == 'upb/master/request')
+        {
+            console.log("[Received] topic: " + topic.toString());  //Print topic name
+            console.log("[Received] message: " + message.toString()); //Print payload
+            let requestedWorker = collection.findOne({},function(err, result){
+                if (err) throw err;
+                console.log(result);
+                return result;
+            });
+            collection.deleteOne({}, function(err, obj) {
+                if (err) throw err;
+                console.log("1 document deleted");
+              });
+            let response = {
+                "destination" : message["sensor_id"],
+                "worker" : requestedWorker["worker_id"]
+                };
+            client.publish('upb/master/response', JSON.stringify(response));
+        }
     });
 });
 
-client.on('message', function (topic, message) { 
-    if(topic == 'upb/master/register')
-    {
-        console.log("[Received] topic: " + topic.toString());  //Print topic name
-        console.log("[Received] message: " + message.toString()); //Print payload
-        PUT(message);
-    }
-    else if(topic == 'upb/master/request')
-    {
-        console.log("[Received] topic: " + topic.toString());  //Print topic name
-        console.log("[Received] message: " + message.toString()); //Print payload
-        let requestedWorker = GET();
-        DELETE();
-        let response = {
-            "destination" : message["sensor_id"],
-            "worker" : requestedWorker["worker_id"]
-            };
-        client.publish('upb/master/response', JSON.stringify(response));
-    }
-})
-
-    function PUT(message){
-        MongoClient.connect(url, function(err, db) {
-            if (err) throw err;
-            var dbo = db.db("tp5-db");
-            var workertoRegister = JSON.parse(message);
-            dbo.collection("workers").insertOne(workertoRegister, function(err, obj) {
-                if (err) throw err;
-                console.log("1 document inserted");
-                db.close();
-        });
-      })
-
-}
-    function GET(){
-        MongoClient.connect(url, function(err, db) {
-            if (err) throw err;
-            var dbo = db.db("tp5-db");
-            dbo.collection("workers").findOne({},function(err, result) {
-              if (err) throw err;
-              console.log(result);
-              return result;
-              db.close();
-            });
-          }); 
-}
-    function DELETE(){
-        MongoClient.connect(url, function(err, db) {
-            if (err) throw err;
-            var dbo = db.db("tp5-db");
-            dbo.collection("workers").deleteOne({}, function(err, obj) {
-              if (err) throw err;
-              console.log("1 document deleted");
-              db.close();
-            });
-          }); 
-}
 
