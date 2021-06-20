62d6ce8769f92924b5300e6ae3921a4185885b5c
stefano Sossi <tenosossi2012@gmail.com>
2021-06-20 15:40:45 -0400

    Pushing grpc first client and server

---
+++ TP6/server_client_gRPC/greeter_server.js
@@ -0,0 +1,67 @@
+/*
+ *
+ * Copyright 2015 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+var PROTO_PATH = __dirname + '/helloworld.proto';
+
+var grpc = require('@grpc/grpc-js');
+var protoLoader = require('@grpc/proto-loader');
+var packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {keepCase: true,
+     longs: String,
+     enums: String,
+     defaults: true,
+     oneofs: true
+    });
+var hello_proto = grpc.loadPackageDefinition(packageDefinition).helloworld;
+
+const maxfreq = 1.5;
+const minfreq = 0.5;
+const maxite = 20;
+const minite = 5;
+/**
+ * Implements the SayHello RPC method.
+ */
+function sayHello(call, callback) {
+  callback(null, {message: 'Hello ' + call.request.name});
+}
+function register(call, callback) {
+  
+  let freq = Math.random() * (maxfreq - minfreq) + minfreq;
+  freq = freq.toFixed(2);
+  let iteration = Math.random() * (maxite - minite) + minite;
+  iteration = Math.round(iteration);
+  
+  let obj = "freq :" + freq + "iteration :" + iteration;
+  console.log("worker id = " +call.request.worker_id);
+  callback(null, {message: obj + call.request.worker_id});
+}
+
+/**
+ * Starts an RPC server that receives requests for the Greeter service at the
+ * sample server port
+ */
+function main() {
+  var server = new grpc.Server();
+  server.addService(hello_proto.Greeter.service, {register: register});
+  server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
+    server.start();
+  });
+}
+
+main();
