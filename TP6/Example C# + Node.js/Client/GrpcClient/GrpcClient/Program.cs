3eee785354b4b08b88aaa12faf3d76742cdeacb1
Vincent Valenzuela <vincent.valenzuela@fundacion-jala.org>
2021-06-21 17:23:18 -0400

    Added a example of working gRPC communication between C# and JS

---
+++ TP6/Example C# + Node.js/Client/GrpcClient/GrpcClient/Program.cs
@@ -0,0 +1,51 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+
+namespace GrpcClient
+{
+    class Program
+    {
+        const string DefaultHost = "localhost";
+        const int Port = 50051;
+        public static void  Main(string[] args)
+        {
+            RunAsync(args).Wait();
+        }
+        private static async Task RunAsync(string[] args)
+        {
+            var host = args.Length == 1 ? args[0] : DefaultHost;
+            var channelTarget = $"{host}:{Port}";
+
+            Console.WriteLine($"Target: {channelTarget}");
+
+            // Create a channel
+             var channel = new Channel (channelTarget , ChannelCredentials.Insecure);
+
+            try
+            {
+                // Create a client with the channel
+                var client = new Greeter.GreeterClient(channel);
+
+                // Create a request
+                var request = new HelloRequest
+                {
+                    Name = "RequestCLI",
+                };
+
+                // Send the request
+                Console.WriteLine("GreeterClient sending request");
+                var response = await client.SayHelloAsync(request);
+
+                Console.WriteLine("GreeterClient received response: " + response.Message);
+            }
+            finally
+            {
+                // Shutdown
+                await channel.ShutdownAsync();
+            }
+        }
+    }
+}
